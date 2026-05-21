e3afed00145431db2bd4bbea4ff2f0a0bae9684b
Vaivaswatha N <vaivaswatha@gmail.com>
2026-05-21 13:39:17 +0530

    Add cfg(not..) in main.rs for the example

--- examples/kaleidoscope/main.rs
+++ examples/kaleidoscope/main.rs
@@ -1,5 +1,8 @@
 //! Entry-point for the kaleidoscope example compiler
 
+// We use pliron-llvm in this test, which is not supported in wasm.
+#![cfg(not(target_family = "wasm"))]
+
 mod ast;
 mod dialect;
 mod from_ast;
