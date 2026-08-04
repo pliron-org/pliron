6d2b90555f59cd3d69091e401bbc9db0870e8f74
Vaivaswatha N <vaivaswatha@gmail.com>
2026-08-04 18:39:24 +0530

    Update pliron-derive too with the new map usages

--- pliron-derive/src/derive_format.rs
+++ pliron-derive/src/derive_format.rs
@@ -3,12 +3,14 @@
 
 use proc_macro2::{Span, TokenStream};
 use quote::{ToTokens, format_ident, quote};
-use rustc_hash::{FxHashMap, FxHashSet};
 use syn::{Data, DeriveInput, Generics, LitStr, Result, spanned::Spanned};
 
-use crate::irfmt::{
-    Directive, Elem, FieldIdent, FmtData, Format, Lit, UnnamedVar, Var, canonical_format_for_enums,
-    canonical_format_for_structs, canonical_op_format,
+use crate::{
+    IMap, ISet,
+    irfmt::{
+        Directive, Elem, FieldIdent, FmtData, Format, Lit, UnnamedVar, Var,
+        canonical_format_for_enums, canonical_format_for_structs, canonical_op_format,
+    },
 };
 
 /// Data parsed from the macro to derive formatting for Rust types
@@ -1070,13 +1072,13 @@ impl ParsableBuilder<()> for DeriveBaseParsable {}
 
 /// Specify various elements individually or all at once.
 enum ElementSpec<T> {
-    Individual(FxHashMap<T, syn::Ident>),
+    Individual(IMap<T, syn::Ident>),
     All(syn::Ident),
 }
 
 impl<T> Default for ElementSpec<T> {
     fn default() -> Self {
-        ElementSpec::Individual(FxHashMap::default())
+        ElementSpec::Individual(IMap::default())
     }
 }
 
@@ -1092,7 +1094,7 @@ struct OpParserState {
     operand_types: ElementSpec<usize>,
     result_types: ElementSpec<usize>,
     // The second element specifies attribtues that are specified as optional.
-    attributes: (ElementSpec<String>, FxHashSet<String>),
+    attributes: (ElementSpec<String>, ISet<String>),
     regions: ElementSpec<usize>,
 }
 
