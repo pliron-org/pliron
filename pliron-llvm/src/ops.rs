3b068529c897fc1d141c080405d2c49a21f98303
Genna Wingert <1058083+wingertge@users.noreply.github.com>
2026-08-13 12:56:40 +0200

    Revert verify

--- pliron-llvm/src/ops.rs
+++ pliron-llvm/src/ops.rs
@@ -62,7 +62,7 @@ use crate::{
     attributes::{
         AddressSpaceAttr, AlignmentAttr, AtomicOrderingAttr, AtomicRmwKindAttr, CaseValuesAttr,
         FCmpPredicateAttr, FastmathFlagsAttr, InsertExtractValueIndicesAttr, LinkageAttr,
-        PoisonAttr, ShuffleVectorMaskAttr, UndefAttr, ZeroAttr,
+        ShuffleVectorMaskAttr,
     },
     op_interfaces::{
         AlignableOpInterface, BinArithOp, CastOpInterface, CastOpWithNNegInterface, FastMathFlags,
@@ -2349,12 +2349,7 @@ impl Verify for ConstantOp {
     fn verify(&self, ctx: &Context) -> Result<()> {
         let loc = self.loc(ctx);
         let value = self.get_value(ctx);
-        if !(value.is::<IntegerAttr>()
-            || attr_impls::<dyn FloatAttr>(&*value)
-            || value.is::<ZeroAttr>()
-            || value.is::<PoisonAttr>()
-            || value.is::<UndefAttr>())
-        {
+        if !(value.is::<IntegerAttr>() || attr_impls::<dyn FloatAttr>(&*value)) {
             verify_err!(loc, ConstantOpVerifyErr::InvalidValue)?;
         }
         Ok(())
